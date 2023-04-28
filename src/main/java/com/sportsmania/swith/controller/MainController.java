cef928149cbd60b13cdb7499afa8da08b8be7716
juyeop <kkang18996@gmail.com>
2023-04-28 21:54:16 +0900

    "temporary"

--- src/main/java/com/sportsmania/swith/controller/MainController.java
+++ src/main/java/com/sportsmania/swith/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.sportsmania.swith.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,8 +12,14 @@ public class MainController {
         return "/main/main";
     }
 
-    @GetMapping("/chat")
+    /*@GetMapping("/chat")
     public String chat(){
         return "/chat/chat";
+    }*/
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/chat")
+    public void chatting(Model model) {
+        model.addAttribute("re", "re");
     }
 }
