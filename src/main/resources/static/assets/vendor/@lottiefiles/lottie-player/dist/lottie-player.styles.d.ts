22a3757e6f30e05ef8df8e8f618886f78a62cf43
juyeop <kkang18996@gmail.com>
2023-05-02 11:06:42 +0900

    gitignore 업뎃

--- src/main/resources/static/assets/vendor/@lottiefiles/lottie-player/dist/lottie-player.styles.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Copyright 2021 Design Barn Inc.
- */
-declare const _default: any;
-export default _default;
-//# sourceMappingURL=lottie-player.styles.d.ts.map
--- src/main/resources/static/assets/vendor/@lottiefiles/lottie-player/dist/tgs-player.styles.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Copyright 2022 Design Barn Inc.
- */
-declare const _default: any;
-export default _default;
-//# sourceMappingURL=tgs-player.styles.d.ts.map
