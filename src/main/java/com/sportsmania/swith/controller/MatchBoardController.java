799d24d4ed5d1c1e0c4b9f6c3a56ef62253db70f
hongsoonho <hongsoonho0723@gmail.com>
2023-05-10 09:30:39 +0900

    매치 상세보기/수정페이지 ver 1.2

--- src/main/java/com/sportsmania/swith/controller/MatchBoardController.java
+++ src/main/java/com/sportsmania/swith/controller/MatchBoardController.java
@@ -19,16 +19,38 @@ public class MatchBoardController {
     @Autowired
     MatchBoardService matchBoardService;
 
-    @RequestMapping("/")
-    public @ResponseBody
-    String root() throws Exception {
-        return "test1212";
+
+
+    @GetMapping("/match/view")
+    public String read(Integer board_no,  String userId, Model model) {
+        if (board_no == null || userId == null) {
+            log.info("board_no:"+board_no);
+            log.info("userId:"+userId);
+            log.info("null발생!!");
+            // 처리할 로직 추가
+        } else {
+            // board_no로 MatchBoardDTO 조회
+            MatchBoardDTO matchBoardDTO = matchBoardService.getOne(board_no);
+            log.info(matchBoardDTO);
+            model.addAttribute("dto", matchBoardDTO);
+
+            // userId로 UserDTO 조회
+            UserDTO userDTO = matchBoardService.getOne1(userId);
+            log.info(userDTO);
+            log.info(userId);
+            model.addAttribute("user", userDTO);
+
+        }
+        return "/match/matching-view";
+
     }
 
 
 
-    @GetMapping("/match/matching-view")
-    public void read(Integer board_no, String userId, Model model) {
+
+
+    @GetMapping("/match/modify")
+    public String read1(Integer board_no, String userId, Model model) {
         if (board_no == null || userId == null) {
             log.info("board_no:"+board_no);
             log.info("userId:"+userId);
@@ -46,34 +68,7 @@ public class MatchBoardController {
             log.info(userId);
             model.addAttribute("user", userDTO);
         }
-    }
-
-
-
-
-
-
-
-
-    @GetMapping("/match/matching-modify")
-    public void read1(Integer board_no, String userId, Model model) {
-        if (board_no == null || userId == null) {
-            log.info("board_no:"+board_no);
-            log.info("userId:"+userId);
-            log.info("null발생!!");
-            // 처리할 로직 추가
-        } else {
-            // board_no로 MatchBoardDTO 조회
-            MatchBoardDTO matchBoardDTO = matchBoardService.getOne(board_no);
-            log.info(matchBoardDTO);
-            model.addAttribute("dto", matchBoardDTO);
-
-            // userId로 UserDTO 조회
-            UserDTO userDTO = matchBoardService.getOne1(userId);
-            log.info(userDTO);
-            log.info(userId);
-            model.addAttribute("user", userDTO);
-        }
+        return "/match/matching-modify";
     }
 
 
@@ -84,18 +79,13 @@ public class MatchBoardController {
 
 
     @PostMapping("/match/matching-modify")
-    public String modify( MatchBoardDTO matchBoardDTO ){
+    public String modify(UserDTO userDTO, MatchBoardDTO matchBoardDTO ){
 
         log.info(matchBoardDTO);
         matchBoardService.modify(matchBoardDTO);
 
-        return "redirect:/";
+        return "redirect:/match/view?board_no=" + matchBoardDTO.getBoard_no() + "&userId=" + userDTO.getUserId();
     }
-
-    @GetMapping("/match/blog-grid")
-    public String test14() {
-        log.info("test1 ok");
-        return "match/blog-grid";}
 
 
 
@@ -104,14 +94,9 @@ public class MatchBoardController {
         log.info("-------------remove--------------");
         log.info("board_no="+board_no);
         matchBoardService.remove(board_no);
-        return "redirect:match/blog-grid";
+        return "redirect:/match";
     }
 
-    @GetMapping("/match/mapapi")
-    public String test15(){
-        log.info("map ok");
-        return "match/mapapi";
-    }
 
 
 }
